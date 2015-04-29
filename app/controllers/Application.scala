6d3b82dfd564be96987cc40f6799159c551f4ed9
Masatoshi TAKAMIYA <gate000jp@gmail.com>
2015-04-29 19:59:49 +0900

    fix websock

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,23 +12,21 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def socket = WebSocket.using[String] { request =>
+  def socket = WebSocket.using[String] {
+    request =>
 
     // Concurrent.broadcast returns (Enumerator, Concurrent.Channel)
     val (out, channel) = Concurrent.broadcast[String]
 
     // log the message to stdout and send response back to client
     val in = Iteratee.foreach[String] {
-      msg => println(msg)
+      msg =>
+        println(msg)
+
         // the Enumerator returned by Concurrent.broadcast subscribes to the channel and will
         // receive the pushed messages
-        
-        // channel push("I received your message: " + msg)
+        channel push("I received your message: " + msg)
     }
-
-    // Send a single 'Hello!' message and close
-    // val out = Enumerator("Hello!").andThen(Enumerator.eof)
-
 
     (in,out)
   }
