eca576072c22a16896a55a4c19f90014676c5969
Masatoshi TAKAMIYA <gate000jp@gmail.com>
2015-04-29 20:46:55 +0900

    implements multiple-user support

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,15 @@
 package controllers
 
 import play.api._
+import play.api.libs.iteratee._
+import play.api.libs.iteratee.Concurrent.Channel
 import play.api.mvc._
 
-import play.api.libs.iteratee._
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object Application extends Controller {
+
+  var connections : List[Channel[String]] = Nil
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
@@ -18,15 +21,22 @@ object Application extends Controller {
     // Concurrent.broadcast returns (Enumerator, Concurrent.Channel)
     val (out, channel) = Concurrent.broadcast[String]
 
-    // log the message to stdout and send response back to client
-    val in = Iteratee.foreach[String] {
-      msg =>
-        println("received message is " + msg)
+    // register
+    connections ::= channel
 
-        // the Enumerator returned by Concurrent.broadcast subscribes to the channel and will
-        // receive the pushed messages
-        channel push("I received your message: " + msg)
-    }
+    // log the message to stdout and send response back to client
+    val in = Iteratee.foreach[String] (msg => {
+      println("received message is " + msg)
+
+      // the Enumerator returned by Concurrent.broadcast subscribes to the channel and will
+      // receive the pushed messages
+      // push is called for each connections
+      connections.foreach(_ push("I received your message: " + msg))
+    }).map(_ => {
+      // unregister
+      connections = connections.filter(_ != channel)
+      println("disconnected")
+    })
 
     (in,out)
   }
